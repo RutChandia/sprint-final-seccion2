2691b51d8cbad3d054821e5af9f73bfe30556004
[Noemi] <[rutchandia@gmail.com]>
2023-04-21 20:53:46 -0400

    funcion para resolver y limpiar valores en el display

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,17 @@
 function inputValue(value){
     document.getElementById('display').value += value
-
     return value
+}
+
+function clean() {
+    SyntaxError = false
+    display.value = "";
+
+}
+
+function result(){
+    let x = document.getElementById('display').value
+    let y = eval(x);
+    document.getElementById('display').value = y
+    return y
 }
